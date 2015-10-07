a39e4b8402b931f572ad618f90707a45480304e4
RazvanHidan <razvan_hidan@yahoo.com>
2015-10-07 13:36:06 +0300

    Parchet

---
+++ Parchet/Parchet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Parchet
+{
+    [TestClass]
+    public class Parchet
+    {
+        [TestMethod]
+        public void TestNumberPieces()
+        {
+            Assert.AreEqual(NumberPieces (5,4,2,2), 6);
+            Assert.AreEqual(NumberPieces (4,4,2,2), 5);
+        }
+        int NumberPieces(int length, int  width, int  lengthPiece, int  widthPiece)
+        {
+            return (int)Math.Ceiling((length * width ) / (lengthPiece * widthPiece * 0.85));
+        }
+    }
+}
